38d24737420345f8a3f04e347e6b70dc879b0c59
Ericgacoki <gacokieric@gmail.com>
2024-03-30 04:27:35 +0300

    enable kotlin symbol processing

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,6 @@
 plugins {
     id("com.android.application") version "8.2.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("com.google.dagger.hilt.android") version "2.44" apply false
+    id("com.google.devtools.ksp") version "1.9.0-1.0.13" apply false
 }
