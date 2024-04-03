c1fc8a7dca4a28099354995afa6f66de1f89815b
Ericgacoki <gacokieric@gmail.com>
2024-04-03 14:45:26 +0300

    add string trimmer ext function

--- uicomponent/src/main/java/com/android/swingmusic/uicomponent/util/Extensions.kt
+++ uicomponent/src/main/java/com/android/swingmusic/uicomponent/util/Extensions.kt
@@ -5,3 +5,7 @@ fun Int.formatDuration(): String {
     val seconds = this % 60
     return "$minutes:$seconds"
 }
+
+fun String.trimString(limit: Int): String {
+    return if (this.length <= limit) this else this.take(limit - 1).plus("...")
+}
