bd013f477161677a7c691f761de1134aa2cbd31b
Ericgacoki <gacokieric@gmail.com>
2024-04-03 14:48:18 +0300

    update deps

--- feature/folder/build.gradle.kts
+++ feature/folder/build.gradle.kts
@@ -54,7 +54,7 @@ dependencies {
     // Core
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.compose.material3:material3")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
