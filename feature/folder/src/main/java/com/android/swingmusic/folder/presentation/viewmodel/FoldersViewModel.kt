eaa26c62420db4bfb6037ba5dd531261af9d544a
Ericgacoki <gacokieric@gmail.com>
2024-04-04 14:49:46 +0300

    handle events, api calls, ui states

--- feature/folder/src/main/java/com/android/swingmusic/folder/presentation/viewmodel/FoldersViewModel.kt
+++ feature/folder/src/main/java/com/android/swingmusic/folder/presentation/viewmodel/FoldersViewModel.kt
@@ -1,10 +1,16 @@
 package com.android.swingmusic.folder.presentation.viewmodel
 
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.android.swingmusic.core.domain.model.Folder
 import com.android.swingmusic.core.domain.model.FoldersAndTracks
 import com.android.swingmusic.core.domain.model.FoldersAndTracksRequest
 import com.android.swingmusic.core.util.Resource
+import com.android.swingmusic.folder.presentation.event.FolderUiEvent
+import com.android.swingmusic.folder.presentation.state.FoldersAndTracksState
 import com.android.swingmusic.network.domain.repository.NetworkRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -15,29 +21,126 @@ import javax.inject.Inject
 class FoldersViewModel @Inject constructor(
     private val networkRepository: NetworkRepository
 ) : ViewModel() {
+    // TODO: Read this from DB
+    private val defaultFolder: Folder = Folder(
+        path = "/home/eric/swing",
+        name = "Home",
+        fileCount = 0,
+        isSym = false
+    )
+    private var _currentFolder: MutableState<Folder> = mutableStateOf(defaultFolder)
+    val currentFolder: State<Folder> = _currentFolder
 
-    // TODO: Make this a flow/state
-    var _foldersAndTracks: List<FoldersAndTracks> = emptyList()
 
-    fun getFoldersAndTracks() {
+    private var _navPaths: MutableState<List<Folder>> =
+        mutableStateOf(listOf(defaultFolder))
+    val navPaths: State<List<Folder>> = _navPaths
+
+    private var _foldersAndTracks: MutableState<FoldersAndTracksState> =
+        mutableStateOf(
+            FoldersAndTracksState(
+                foldersAndTracks = FoldersAndTracks(
+                    folders = emptyList(),
+                    tracks = emptyList()
+                ), isLoading = true, isError = false
+            )
+        )
+
+    val foldersAndTracks: State<FoldersAndTracksState> = _foldersAndTracks
+
+    private fun resetUiStates() {
+        _foldersAndTracks.value = FoldersAndTracksState(
+            foldersAndTracks = FoldersAndTracks(
+                folders = emptyList(),
+                tracks = emptyList()
+            ),
+            isLoading = true,
+            isError = false
+        )
+    }
+
+    private fun getFoldersAndTracks(path: String) {
+        // resetUiStates()
         viewModelScope.launch {
-            // TODO: Replace this with the path provided by click events
-            val path = "/home/eric/swing"
             val request = FoldersAndTracksRequest(path, false)
 
-            when (networkRepository.getFoldersAndTracks(request)) {
+            when (val result = networkRepository.getFoldersAndTracks(request)) {
                 is Resource.Success -> {
-                    Timber.e("Getting folders...")
+                    _foldersAndTracks.value = FoldersAndTracksState(
+                        foldersAndTracks = result.data ?: FoldersAndTracks(
+                            emptyList(),
+                            emptyList()
+                        ),
+                        isLoading = false,
+                        isError = false
+                    )
                 }
 
-                else -> {
-                    Timber.e("Error Getting folders...")
+                is Resource.Error -> {
+                    _foldersAndTracks.value =
+                        _foldersAndTracks.value.copy(
+                            foldersAndTracks = FoldersAndTracks(
+                                emptyList(),
+                                emptyList()
+                            ),
+                            isLoading = false,
+                            isError = true,
+                            errorMsg = result.message ?: "Unable to fetch folders"
+                        )
+                }
+
+                is Resource.Loading -> {
+                    _foldersAndTracks.value =
+                        _foldersAndTracks.value.copy(
+                            foldersAndTracks = FoldersAndTracks(
+                                emptyList(),
+                                emptyList()
+                            ),
+                            isLoading = true,
+                            isError = false
+                        )
                 }
             }
         }
     }
 
     init {
-        Timber.e("View model init...")
+        getFoldersAndTracks(path = defaultFolder.path)
+    }
+
+    fun onFolderUiEvent(event: FolderUiEvent) {
+        when (event) {
+            is FolderUiEvent.ClickNavPath -> {
+                _currentFolder.value = event.folder
+                getFoldersAndTracks(event.folder.path)
+            }
+
+            is FolderUiEvent.ClickFolder -> {
+                _currentFolder.value = event.folder
+                getFoldersAndTracks(event.folder.path)
+
+                _navPaths.value =
+                    _navPaths.value.filter {
+                        event.folder.path.contains(it.path)
+                    }.plus(event.folder).toSet().toList()
+            }
+
+            is FolderUiEvent.OnBackNav -> {
+                if (_navPaths.value.size > 1) {
+                    val currentPathIndex = _navPaths.value.indexOf(event.folder)
+                    val backPathIndex = currentPathIndex - 1
+                    if (backPathIndex > -1) { // Just to be safe
+                        val backFolder = _navPaths.value[backPathIndex]
+                        _currentFolder.value = backFolder
+                        getFoldersAndTracks(backFolder.path)
+                    }
+                }
+            }
+
+            is FolderUiEvent.Retry -> {
+                resetUiStates()
+                getFoldersAndTracks(_currentFolder.value.path)
+            }
+        }
     }
 }
