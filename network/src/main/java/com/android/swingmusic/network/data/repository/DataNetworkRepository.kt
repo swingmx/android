be5642c2b88a037b18a19deba45b7f3f8d136487
Ericgacoki <gacokieric@gmail.com>
2024-04-05 23:32:06 +0300

    move packages to respective layers

--- network/src/main/java/com/android/swingmusic/network/data/repository/DataNetworkRepository.kt
+++ network/src/main/java/com/android/swingmusic/network/data/repository/DataNetworkRepository.kt
@@ -2,8 +2,10 @@ package com.android.swingmusic.network.data.repository
 
 import com.android.swingmusic.core.data.mapper.Map.toFolderAndTracks
 import com.android.swingmusic.core.data.mapper.Map.toFoldersAndTracksRequestDto
+import com.android.swingmusic.core.data.mapper.Map.toRootDirs
 import com.android.swingmusic.core.domain.model.FoldersAndTracks
 import com.android.swingmusic.core.domain.model.FoldersAndTracksRequest
+import com.android.swingmusic.core.domain.model.RootDirs
 import com.android.swingmusic.core.util.Resource
 import com.android.swingmusic.network.data.api.ApiService
 import com.android.swingmusic.network.domain.repository.NetworkRepository
@@ -14,6 +16,23 @@ import javax.inject.Inject
 class DataNetworkRepository @Inject constructor(
     private val apiService: ApiService,
 ) : NetworkRepository {
+    override suspend fun getRootDirectories(): Resource<RootDirs> {
+        return try {
+            val rootDirs = apiService.getRootDirectories()
+            Resource.Success(data = rootDirs.toRootDirs())
+        } catch (e: IOException) {
+            Resource.Error(
+                message = e.localizedMessage
+                    ?: "Unable to fetch root directories\nCheck your connection and try again!"
+            )
+
+        } catch (e: HttpException) {
+            Resource.Error(
+                message = e.localizedMessage ?: "An unexpected error occurred!"
+            )
+        }
+    }
+
     override suspend fun getFoldersAndTracks(requestData: FoldersAndTracksRequest): Resource<FoldersAndTracks> {
         return try {
             val foldersAndTracksDto =
