5f0ec95360d47582b4116aea7c2df5d6eb4a07f3
Ericgacoki <gacokieric@gmail.com>
2024-04-05 23:33:16 +0300

    get root dir (the hard way)

--- network/src/main/java/com/android/swingmusic/network/data/api/ApiService.kt
+++ network/src/main/java/com/android/swingmusic/network/data/api/ApiService.kt
@@ -2,11 +2,15 @@ package com.android.swingmusic.network.data.api
 
 import com.android.swingmusic.core.data.dto.FoldersAndTracksDto
 import com.android.swingmusic.core.data.dto.FoldersAndTracksRequestDto
+import com.android.swingmusic.core.data.dto.RootDirsDto
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface ApiService {
+    @GET("/notsettings/get-root-dirs")
+    suspend fun getRootDirectories(): RootDirsDto
+
     @POST("/folder")
     suspend fun getFoldersAndTracks(@Body requestData: FoldersAndTracksRequestDto): FoldersAndTracksDto
-
 }
