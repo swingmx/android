89291dd5453a133c6642f88743dfdb030a5fbddb
Ericgacoki <gacokieric@gmail.com>
2024-04-09 15:24:04 +0300

    clear imports

--- uicomponent/src/main/java/com/android/swingmusic/uicomponent/presentation/component/FolderItem.kt
+++ uicomponent/src/main/java/com/android/swingmusic/uicomponent/presentation/component/FolderItem.kt
@@ -20,7 +20,6 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.MoreVert
-import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
