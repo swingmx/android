1c43f1eb189bc7d8f9eedf1d0a5f81ac8e9c3d1f
Ericgacoki <gacokieric@gmail.com>
2024-04-28 22:27:36 +0300

    remove league spartan font

--- uicomponent/src/main/java/com/android/swingmusic/uicomponent/presentation/theme/Type.kt
+++ uicomponent/src/main/java/com/android/swingmusic/uicomponent/presentation/theme/Type.kt
@@ -1,105 +1,78 @@
 package com.android.swingmusic.uicomponent.presentation.theme
 
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
-import com.android.swingmusic.uicomponent.R
-
-val leagueSpartan = FontFamily(
-    Font(R.font.league_spartan_regular, weight = FontWeight.Normal),
-    Font(R.font.league_spartan_light, weight = FontWeight.Light),
-    Font(R.font.league_spartan_thin, weight = FontWeight.Thin),
-    Font(R.font.league_spartan_medium, weight = FontWeight.Medium),
-    Font(R.font.league_spartan_semi_bold, weight = FontWeight.SemiBold),
-    Font(R.font.league_spartan_bold, weight = FontWeight.Bold),
-    Font(R.font.league_spartan_black, weight = FontWeight.Black),
-    Font(R.font.league_spartan_extra_bold, weight = FontWeight.ExtraBold),
-    Font(R.font.league_spartan_extra_light, weight = FontWeight.ExtraLight),
-)
 
 val Typography = androidx.compose.material3.Typography(
     headlineLarge = TextStyle(
         fontWeight = FontWeight.SemiBold,
-        fontFamily = leagueSpartan,
         fontSize = 32.sp,
         lineHeight = 40.sp,
         letterSpacing = 0.sp
     ),
     headlineMedium = TextStyle(
         fontWeight = FontWeight.SemiBold,
-        fontFamily = leagueSpartan,
         fontSize = 28.sp,
         lineHeight = 36.sp,
         letterSpacing = 0.sp
     ),
     headlineSmall = TextStyle(
         fontWeight = FontWeight.SemiBold,
-        fontFamily = leagueSpartan,
         fontSize = 24.sp,
         lineHeight = 32.sp,
         letterSpacing = 0.sp
     ),
     titleLarge = TextStyle(
         fontWeight = FontWeight.SemiBold,
-        fontFamily = leagueSpartan,
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
     titleMedium = TextStyle(
         fontWeight = FontWeight.SemiBold,
-        fontFamily = leagueSpartan,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.15.sp
     ),
     titleSmall = TextStyle(
         fontWeight = FontWeight.Bold,
-        fontFamily = leagueSpartan,
         fontSize = 14.sp,
         lineHeight = 20.sp,
         letterSpacing = 0.1.sp
     ),
     bodyLarge = TextStyle(
         fontWeight = FontWeight.Normal,
-        fontFamily = leagueSpartan,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.15.sp
     ),
     bodyMedium = TextStyle(
         fontWeight = FontWeight.Normal,
-        fontFamily = leagueSpartan,
         fontSize = 14.sp,
         lineHeight = 20.sp,
         letterSpacing = 0.25.sp
     ),
     bodySmall = TextStyle(
         fontWeight = FontWeight.Normal,
-        fontFamily = leagueSpartan,
         fontSize = 12.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.4.sp
     ),
     labelLarge = TextStyle(
         fontWeight = FontWeight.SemiBold,
-        fontFamily = leagueSpartan,
         fontSize = 14.sp,
         lineHeight = 20.sp,
         letterSpacing = 0.1.sp
     ),
     labelMedium = TextStyle(
         fontWeight = FontWeight.SemiBold,
-        fontFamily = leagueSpartan,
         fontSize = 12.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
     ),
     labelSmall = TextStyle(
         fontWeight = FontWeight.SemiBold,
-        fontFamily = leagueSpartan,
         fontSize = 11.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
