23d21da147143a75537d96e7f8dd55c54fe33930
Ericgacoki <gacokieric@gmail.com>
2024-04-30 10:22:14 +0300

    default url to mungaist

--- network/src/main/java/com/android/swingmusic/network/data/util/Constants.kt
+++ network/src/main/java/com/android/swingmusic/network/data/util/Constants.kt
@@ -3,5 +3,5 @@ package com.android.swingmusic.network.data.util
 import org.jetbrains.annotations.TestOnly
 
 @TestOnly // TODO: Get this from DB
-const val BASE_URL = "http://192.168.85.41:1970"
-// const val BASE_URL = "https://music.mungaist.com/"
+// const val BASE_URL = "http://192.168.85.41:1970"
+const val BASE_URL = "https://music.mungaist.com/"
