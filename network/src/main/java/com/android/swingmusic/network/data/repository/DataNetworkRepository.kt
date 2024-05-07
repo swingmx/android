2139bbbad2dd5304e0351d182b0afd5ab4d96a6d
Ericgacoki <gacokieric@gmail.com>
2024-05-07 13:34:03 +0300

    add paging lib

--- network/src/main/java/com/android/swingmusic/network/data/repository/DataNetworkRepository.kt
+++ network/src/main/java/com/android/swingmusic/network/data/repository/DataNetworkRepository.kt
@@ -1,14 +1,19 @@
 package com.android.swingmusic.network.data.repository
 
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import com.android.swingmusic.core.data.mapper.Map.toAllArtists
 import com.android.swingmusic.core.data.mapper.Map.toFolderAndTracks
 import com.android.swingmusic.core.data.mapper.Map.toFoldersAndTracksRequestDto
-import com.android.swingmusic.core.data.mapper.Map.toRootDirs
+import com.android.swingmusic.core.data.util.Resource
+import com.android.swingmusic.core.domain.model.Artist
 import com.android.swingmusic.core.domain.model.FoldersAndTracks
 import com.android.swingmusic.core.domain.model.FoldersAndTracksRequest
-import com.android.swingmusic.core.domain.model.RootDirs
-import com.android.swingmusic.core.data.util.Resource
-import com.android.swingmusic.network.data.api.ApiService
+import com.android.swingmusic.network.data.api.paging.ArtistsSource
+import com.android.swingmusic.network.data.api.service.ApiService
 import com.android.swingmusic.network.domain.repository.NetworkRepository
+import kotlinx.coroutines.flow.Flow
 import retrofit2.HttpException
 import java.io.IOException
 import javax.inject.Inject
@@ -18,10 +23,10 @@ class DataNetworkRepository @Inject constructor(
 ) : NetworkRepository {
     override suspend fun getFoldersAndTracks(requestData: FoldersAndTracksRequest): Resource<FoldersAndTracks> {
         return try {
+            Resource.Loading<FoldersAndTracks>()
             val foldersAndTracksDto =
                 apiService.getFoldersAndTracks(requestData.toFoldersAndTracksRequestDto())
             Resource.Success(data = foldersAndTracksDto.toFolderAndTracks())
-
         } catch (e: IOException) {
             Resource.Error(
                 message = e.localizedMessage
@@ -32,6 +37,25 @@ class DataNetworkRepository @Inject constructor(
             Resource.Error(
                 message = e.localizedMessage ?: "An unexpected error occurred!"
             )
+        }
+    }
+
+    override fun getPagingArtists(sortBy: String, sortOrder: Int): Flow<PagingData<Artist>> {
+        return Pager(
+            config = PagingConfig(enablePlaceholders = false, pageSize = 20),
+            pagingSourceFactory = {
+                ArtistsSource(api = apiService, sortBy = sortBy, sortOrder = sortOrder)
+            }
+        ).flow
+    }
+
+    override suspend fun getArtistsCount(): Resource<Int> {
+        return try {
+            Resource.Loading<Int>()
+
+            Resource.Success(data = apiService.getSampleArtist().toAllArtists().total)
+        } catch (e: Exception) {
+            Resource.Error(message = "Error loading artists")
         }
     }
 }
