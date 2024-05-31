df720e49b1b1c96e4561e2866f03eede8ee8ae55
Ericgacoki <gacokieric@gmail.com>
2024-05-31 12:24:46 +0300

    add PlayerUiEvents

---
+++ app/src/main/java/com/android/swingmusic/presentation/event/PlayerUiEvent.kt
@@ -0,0 +1,26 @@
+package com.android.swingmusic.presentation.event
+
+interface PlayerUiEvent {
+
+    data class OnSeekPlayBack(val value: Float): PlayerUiEvent
+
+    object OnPrev: PlayerUiEvent
+
+    object OnTogglePlayerState: PlayerUiEvent
+
+    object OnNext: PlayerUiEvent
+
+    object OnPlayBackComplete: PlayerUiEvent
+
+    object OnToggleFavorite: PlayerUiEvent
+
+    object OnClickLyricsIcon: PlayerUiEvent
+
+    object OnToggleRepeatMode: PlayerUiEvent
+
+    object OnToggleShuffleMode: PlayerUiEvent
+
+    object OnClickQueue: PlayerUiEvent
+
+    object OnClickMore: PlayerUiEvent
+}
