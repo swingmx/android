b45ee0bf813b1f0a5de2d9074e0dc18b9b539645
Ericgacoki <gacokieric@gmail.com>
2024-06-07 01:41:01 +0300

    swipe MiniPlayer to seek next/prev

--- feature/player/src/main/java/com/android/swingmusic/player/presentation/compose/MiniPlayer.kt
+++ feature/player/src/main/java/com/android/swingmusic/player/presentation/compose/MiniPlayer.kt
@@ -3,6 +3,8 @@ package com.android.swingmusic.player.presentation.compose
 import android.content.res.Configuration
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectHorizontalDragGestures
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -12,6 +14,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
@@ -22,11 +25,15 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -55,105 +62,126 @@ private fun MiniPlayer(
     playbackProgress: Float, // also called seekPosition
     onClickPlayerItem: (hash: String) -> Unit,
     onTogglePlaybackState: () -> Unit,
+    onResumePlayBackFromError: () -> Unit,
+    onSwipeLeft: () -> Unit, //  <<- |
+    onSwipeRight: () -> Unit, // | ->>
 ) {
-    SwingMusicTheme {
-        Surface {
-            Column(
+    Surface {
+        var swipeDistance by remember { mutableFloatStateOf(0F) }
+        val interactions = remember { MutableInteractionSource() }
+
+        Column(
+            modifier = Modifier
+                .clip(RoundedCornerShape(topStart = 12.dp, topEnd = 12.dp))
+                .background(MaterialTheme.colorScheme.inverseOnSurface)
+        ) {
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically,
                 modifier = Modifier
-                    .clip(RoundedCornerShape(topStart = 12.dp, topEnd = 12.dp))
-                    .background(MaterialTheme.colorScheme.inverseOnSurface)
-                    .clickable {
+                    .fillMaxWidth()
+                    .wrapContentHeight()
+                    .pointerInput(Unit) {
+                        detectHorizontalDragGestures(
+                            onDragEnd = {
+                                if (swipeDistance > 50) {
+                                    onSwipeRight()
+                                } else if (swipeDistance < -50) {
+                                    onSwipeLeft()
+                                }
+                                swipeDistance = 0f
+                            }
+                        ) { change, dragAmount ->
+                            change.consume()
+                            swipeDistance += dragAmount
+                        }
+                    }
+                    //.offset { IntOffset(swipeDistance.roundToInt(), 0) }
+                    .clickable(
+                        interactionSource = interactions,
+                        indication = null
+                    ) {
                         onClickPlayerItem(trackHash)
                     }
             ) {
+                // Image, Title
                 Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.SpaceBetween,
+                    modifier = Modifier
+                        .fillMaxWidth(0.8F)
+                        .padding(8.dp),
                     verticalAlignment = Alignment.CenterVertically
                 ) {
-                    // Image, Title
-                    Row(
+                    AsyncImage(
                         modifier = Modifier
-                            .fillMaxWidth(0.8F)
-                            .padding(8.dp),
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        AsyncImage(
-                            modifier = Modifier
-                                .size(36.dp)
-                                .clip(RoundedCornerShape(18)),
-                            model = ImageRequest.Builder(LocalContext.current)
-                                .data("$BASE_URL/img/t/s/${trackImage}")
-                                .crossfade(true)
-                                .build(),
-                            placeholder = painterResource(R.drawable.audio_fallback),
-                            fallback = painterResource(R.drawable.audio_fallback),
-                            error = painterResource(R.drawable.audio_fallback),
-                            contentDescription = "Track Image",
-                            contentScale = ContentScale.Crop
-                        )
+                            .size(38.dp)
+                            .clip(RoundedCornerShape(18)),
+                        model = ImageRequest.Builder(LocalContext.current)
+                            .data("$BASE_URL/img/t/s/${trackImage}")
+                            .crossfade(true)
+                            .build(),
+                        placeholder = painterResource(R.drawable.audio_fallback),
+                        fallback = painterResource(R.drawable.audio_fallback),
+                        error = painterResource(R.drawable.audio_fallback),
+                        contentDescription = "Track Image",
+                        contentScale = ContentScale.Crop
+                    )
 
-                        Spacer(modifier = Modifier.width(8.dp))
+                    Spacer(modifier = Modifier.width(8.dp))
 
-                        Text(
-                            text = trackTitle,
-                            maxLines = 1,
-                            style = MaterialTheme.typography.bodyMedium,
-                            fontWeight = FontWeight.SemiBold,
-                            overflow = TextOverflow.Ellipsis,
-                            color = MaterialTheme.colorScheme.onSurface.copy(alpha = .84F)
+                    Text(
+                        text = trackTitle,
+                        maxLines = 1,
+                        style = MaterialTheme.typography.bodyMedium,
+                        fontWeight = FontWeight.SemiBold,
+                        overflow = TextOverflow.Ellipsis,
+                        color = MaterialTheme.colorScheme.onSurface.copy(alpha = .84F)
+                    )
+                }
+
+                // Player State Indicator
+                IconButton(
+                    modifier = Modifier
+                        .padding(end = 8.dp),
+                    onClick = {
+                        if (playbackState == PlaybackState.ERROR) {
+                            onResumePlayBackFromError()
+                        } else {
+                            onTogglePlaybackState()
+                        }
+                    }
+                ) {
+                    if (isBuffering) {
+                        CircularProgressIndicator(
+                            modifier = Modifier.size(24.dp),
+                            strokeWidth = (.75).dp,
+                            strokeCap = StrokeCap.Round
                         )
                     }
-
-                    // Player State Indicator
-                    IconButton(
-                        modifier = Modifier
-                            .padding(end = 8.dp),
-                        onClick = {
-                            if (playbackState != PlaybackState.ERROR)
-                                onTogglePlaybackState()
-                        }) {
-                        if (isBuffering) {
-                            CircularProgressIndicator(
-                                modifier = Modifier.size(24.dp),
-                                strokeWidth = (.75).dp,
-                                strokeCap = StrokeCap.Round
+                    when (playbackState) {
+                        PlaybackState.PLAYING -> {
+                            Icon(
+                                painter = painterResource(id = R.drawable.pause_icon),
+                                contentDescription = "playing state indicator"
                             )
                         }
-                        when (playbackState) {
-                            PlaybackState.PLAYING -> {
-                                Icon(
-                                    painter = painterResource(id = R.drawable.pause_icon),
-                                    contentDescription = "playing state indicator"
-                                )
-                            }
-
-                            PlaybackState.PAUSED -> {
-                                Icon(
-                                    painter = painterResource(id = R.drawable.play_arrow),
-                                    contentDescription = "paused state indicator"
-                                )
-                            }
-
-                            PlaybackState.ERROR -> {
-                                Icon(
-                                    painter = painterResource(id = R.drawable.error),
-                                    tint = MaterialTheme.colorScheme.inverseSurface.copy(alpha = .5F),
-                                    contentDescription = "paused state indicator"
-                                )
-                            }
+                        // PAUSED, ERROR
+                        else -> {
+                            Icon(
+                                painter = painterResource(id = R.drawable.play_arrow),
+                                contentDescription = "paused state indicator"
+                            )
                         }
                     }
                 }
-
-                LinearProgressIndicator(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(1.dp),
-                    progress = playbackProgress,
-                    strokeCap = StrokeCap.Square
-                )
             }
+
+            LinearProgressIndicator(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(1.dp),
+                progress = playbackProgress,
+                strokeCap = StrokeCap.Square
+            )
         }
     }
 }
@@ -166,39 +194,54 @@ private fun MiniPlayer(
 @Composable
 fun MiniPlayer(mediaControllerViewModel: MediaControllerViewModel = viewModel()) {
     val playerUiState by remember { mediaControllerViewModel.playerUiState }
-    playerUiState.track?.let { track ->
-        SwingMusicTheme {
-            MiniPlayer(
-                trackHash = track.trackHash,
-                trackTitle = track.title,
-                trackImage = track.image,
-                playbackState = playerUiState.playbackState,
-                isBuffering = playerUiState.isBuffering,
-                playbackProgress = playerUiState.seekPosition,
-                onClickPlayerItem = {
-                    // TODO: Open FullScreen player in a full screen bottom sheet
-                },
-                onTogglePlaybackState = {
-                    mediaControllerViewModel.onPlayerUiEvent(PlayerUiEvent.OnTogglePlayerState)
-                }
-            )
-        }
+    playerUiState.nowPlayingTrack?.let { track ->
+        MiniPlayer(
+            trackHash = track.trackHash,
+            trackTitle = track.title,
+            trackImage = track.image,
+            playbackState = playerUiState.playbackState,
+            isBuffering = playerUiState.isBuffering,
+            playbackProgress = playerUiState.seekPosition,
+            onClickPlayerItem = {
+// TODO: Open FullScreen player in a full screen bottom sheet
+            },
+            onTogglePlaybackState = {
+                mediaControllerViewModel.onPlayerUiEvent(PlayerUiEvent.OnTogglePlayerState)
+            },
+            onSwipeLeft = {
+                mediaControllerViewModel.onPlayerUiEvent(PlayerUiEvent.OnNext)
+            },
+            onSwipeRight = {
+                mediaControllerViewModel.onPlayerUiEvent(PlayerUiEvent.OnPrev)
+            },
+            onResumePlayBackFromError = {
+                mediaControllerViewModel.onPlayerUiEvent(PlayerUiEvent.OnResumePlaybackFromError)
+            }
+        )
     }
 }
 
-@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, device = Devices.NEXUS_5, showBackground = true)
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    device = Devices.NEXUS_5,
+    showBackground = true,
+)
 @Composable
 fun MiniPlayerPreview() {
-    SwingMusicTheme {
+    SwingMusicTheme(
+        navBarColor = Color.Red
+    ) {
         MiniPlayer(
-            playbackState = PlaybackState.PLAYING,
-            isBuffering = true,
             trackHash = "abc123",
             trackTitle = "Track title is too large to be displayed",
             trackImage = "https://image",
+            playbackState = PlaybackState.PLAYING,
+            isBuffering = true,
             playbackProgress = 0.2F,
             onClickPlayerItem = {},
-            onTogglePlaybackState = {}
-        )
+            onTogglePlaybackState = {},
+            onResumePlayBackFromError = {},
+            {}
+        ) {}
     }
 }
