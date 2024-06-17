6703915a0e52a1bb9379f549025da889b1048ddd
Ericgacoki <gacokieric@gmail.com>
2024-06-17 21:31:16 +0300

    add base url entity

--- database/src/main/java/com/android/swingmusic/database/data/database/AppDatabase.kt
+++ database/src/main/java/com/android/swingmusic/database/data/database/AppDatabase.kt
@@ -4,18 +4,23 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.android.swingmusic.database.data.converter.Converters
+import com.android.swingmusic.database.data.dao.BaseUrlDao
 import com.android.swingmusic.database.data.dao.LastPlayedTrackDao
 import com.android.swingmusic.database.data.dao.QueueDao
+import com.android.swingmusic.database.data.entity.BaseUrlEntity
 import com.android.swingmusic.database.data.entity.LastPlayedTrackEntity
 import com.android.swingmusic.database.data.entity.QueueEntity
 
 @Database(
-    entities = [QueueEntity::class, LastPlayedTrackEntity::class],
+    entities = [QueueEntity::class, LastPlayedTrackEntity::class, BaseUrlEntity::class],
     version = 1,
     exportSchema = false
 )
 @TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
+    abstract fun baseUrlDao(): BaseUrlDao
+
     abstract fun queueDao(): QueueDao
+
     abstract fun lastPlayedTrackDao(): LastPlayedTrackDao
 }
