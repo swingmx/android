c11348d0eb3495992cf7a85ce285ac71ae53a88b
Ericgacoki <gacokieric@gmail.com>
2024-06-19 22:06:37 +0300

    create user result

---
+++ auth/src/main/java/com/android/swingmusic/auth/data/dto/CreateUserResultDto.kt
@@ -0,0 +1,20 @@
+package com.android.swingmusic.auth.data.dto
+
+import com.google.gson.annotations.SerializedName
+
+data class CreateUserResultDto(
+    @SerializedName("email")
+    val email: String?,
+    @SerializedName("firstname")
+    val firstname: String?,
+    @SerializedName("id")
+    val id: Int?,
+    @SerializedName("image")
+    val image: String?,
+    @SerializedName("lastname")
+    val lastname: String?,
+    @SerializedName("roles")
+    val roles: List<String>?,
+    @SerializedName("username")
+    val username: String?
+)
