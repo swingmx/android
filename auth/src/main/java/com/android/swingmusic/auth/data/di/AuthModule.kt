61f9e525072acfd1d7758c4230823a3ae18eaab2
Ericgacoki <gacokieric@gmail.com>
2024-06-19 21:33:37 +0300

    inject auth token data store

--- auth/src/main/java/com/android/swingmusic/auth/data/di/AuthModule.kt
+++ auth/src/main/java/com/android/swingmusic/auth/data/di/AuthModule.kt
@@ -1,12 +1,16 @@
 package com.android.swingmusic.auth.data.di
 
+
+import android.content.Context
+import com.android.swingmusic.auth.data.datastore.AuthTokensDataStore
+import com.android.swingmusic.auth.data.datastore.SecureStore
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import java.security.KeyStore
 import javax.inject.Singleton
-
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -19,5 +23,13 @@ object AuthModule {
         keyStore.load(null)
         return keyStore
     }
-}
 
+    @Provides
+    @Singleton
+    fun providesAuthTokenDataStore(
+        @ApplicationContext context: Context,
+        secureStore: SecureStore
+    ): AuthTokensDataStore {
+        return AuthTokensDataStore(context = context.applicationContext, secureStore = secureStore)
+    }
+}
