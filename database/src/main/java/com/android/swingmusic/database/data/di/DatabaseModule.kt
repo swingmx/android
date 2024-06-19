803bd7f48d86c7f7352bc45063bba6e775e272e5
Ericgacoki <gacokieric@gmail.com>
2024-06-19 21:33:56 +0300

    inject base url dao

--- database/src/main/java/com/android/swingmusic/database/data/di/DatabaseModule.kt
+++ database/src/main/java/com/android/swingmusic/database/data/di/DatabaseModule.kt
@@ -2,6 +2,7 @@ package com.android.swingmusic.database.data.di
 
 import android.content.Context
 import androidx.room.Room
+import com.android.swingmusic.database.data.dao.BaseUrlDao
 import com.android.swingmusic.database.data.dao.LastPlayedTrackDao
 import com.android.swingmusic.database.data.dao.QueueDao
 import com.android.swingmusic.database.data.database.AppDatabase
@@ -24,6 +25,11 @@ object DatabaseModule {
             AppDatabase::class.java,
             "app_database"
         ).build()
+    }
+
+    @Provides
+    fun provideBaseUrlDao(database: AppDatabase): BaseUrlDao {
+        return database.baseUrlDao()
     }
 
     @Provides
