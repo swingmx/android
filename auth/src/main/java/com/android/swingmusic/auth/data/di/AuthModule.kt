86a7c6b410a8f2e70c07d3b38fee4ce1e11ee298
Ericgacoki <gacokieric@gmail.com>
2024-07-15 01:36:28 +0300

    provide OkHttp via hilt module

--- auth/src/main/java/com/android/swingmusic/auth/data/di/AuthModule.kt
+++ auth/src/main/java/com/android/swingmusic/auth/data/di/AuthModule.kt
@@ -4,7 +4,6 @@ package com.android.swingmusic.auth.data.di
 import android.content.Context
 import com.android.swingmusic.auth.data.api.service.AuthApiService
 import com.android.swingmusic.auth.data.datastore.AuthTokensDataStore
-import com.android.swingmusic.auth.data.datastore.SecureStore
 import com.android.swingmusic.database.data.dao.BaseUrlDao
 import dagger.Module
 import dagger.Provides
@@ -18,33 +17,22 @@ import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import timber.log.Timber
-import java.security.KeyStore
 import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AuthModule {
-
-    @Provides
-    @Singleton
-    fun provideKeyStore(): KeyStore {
-        val keyStore = KeyStore.getInstance("AndroidKeyStore")
-        keyStore.load(null)
-        return keyStore
-    }
-
     @Provides
     @Singleton
     fun providesAuthTokenDataStore(
-        @ApplicationContext context: Context,
-        secureStore: SecureStore
+        @ApplicationContext context: Context
     ): AuthTokensDataStore {
-        return AuthTokensDataStore(context = context.applicationContext, secureStore = secureStore)
+        return AuthTokensDataStore(context = context.applicationContext)
     }
 
-    @Singleton
     @Provides
+    @Singleton
     fun providesOkHttpClient(): OkHttpClient {
         val interceptor = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
         return OkHttpClient.Builder()
