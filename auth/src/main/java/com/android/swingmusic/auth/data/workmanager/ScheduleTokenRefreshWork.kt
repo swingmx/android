eda264a27faf2abb086a051ade05742e0f6d099c
Ericgacoki <gacokieric@gmail.com>
2024-07-24 00:32:12 +0300

    schedule periodic worker

---
+++ auth/src/main/java/com/android/swingmusic/auth/data/workmanager/ScheduleTokenRefreshWork.kt
@@ -0,0 +1,28 @@
+package com.android.swingmusic.auth.data.workmanager
+
+import android.content.Context
+import androidx.work.Constraints
+import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.NetworkType
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkManager
+import java.util.concurrent.TimeUnit
+
+fun scheduleTokenRefreshWork(context: Context) {
+    val constraints = Constraints.Builder()
+        .setRequiredNetworkType(NetworkType.CONNECTED)
+        .setRequiresBatteryNotLow(true)
+        .build()
+
+    val tokenRefreshWorkRequest =
+        PeriodicWorkRequestBuilder<TokenRefreshWorker>(21, TimeUnit.DAYS)
+            .setConstraints(constraints)
+            .build()
+
+    WorkManager.getInstance(context)
+        .enqueueUniquePeriodicWork(
+            TokenRefreshWorker.WORK_NAME,
+            ExistingPeriodicWorkPolicy.KEEP,
+            tokenRefreshWorkRequest
+        )
+}
