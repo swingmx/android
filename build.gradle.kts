3d320a44942f471dad74c7afa6d5db1eb08a3b11
Ericgacoki <gacokieric@gmail.com>
2025-01-21 11:49:43 +0300

    add search to local modules

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,3 @@
-import dev.iurysouza.modulegraph.LinkText
-import dev.iurysouza.modulegraph.Theme
-
-
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.2.1" apply false
@@ -10,12 +6,14 @@ plugins {
     id("com.google.devtools.ksp") version "1.9.0-1.0.13" apply false
     id("com.android.library") version "8.2.1" apply false
 
-    id("dev.iurysouza.modulegraph") version "0.10.0"
+    //  id("dev.iurysouza.modulegraph") version "0.10.0"
 }
 
+/*
 moduleGraphConfig {
     readmePath.set("./README.md")
     heading = "### Module Graph"
     setStyleByModuleType.set(true)
     theme.set(Theme.DARK)
 }
+*/
