2e9493051cce870700161a419fd2236368802f2f
Ericgacoki <gacokieric@gmail.com>
2025-05-04 14:11:44 +0300

    refresh base url after the first composition

--- feature/home/src/main/java/com/android/swingmusic/home/presentation/Home.kt
+++ feature/home/src/main/java/com/android/swingmusic/home/presentation/Home.kt
@@ -4,14 +4,22 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import com.android.swingmusic.player.presentation.viewmodel.MediaControllerViewModel
 import com.android.swingmusic.uicomponent.presentation.theme.SwingMusicTheme
 import com.ramcosta.composedestinations.annotation.Destination
 
 @Destination
 @Composable
-fun Home() {
+fun Home(
+    mediaControllerViewModel: MediaControllerViewModel
+) {
+    SideEffect {
+        mediaControllerViewModel.refreshBaseUrl()
+    }
+
     SwingMusicTheme {
         Box(
             contentAlignment = Alignment.Center,
